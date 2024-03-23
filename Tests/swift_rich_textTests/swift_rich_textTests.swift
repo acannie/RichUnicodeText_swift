4f92e26605b9a332363ee8d058c5609f9d734178
asasaoka <asasaoka@yahoo-corp.jp>
2024-03-24 01:10:21 +0900

    env: プロジェクトを作成

---
+++ Tests/swift_rich_textTests/swift_rich_textTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import swift_rich_text
+
+final class swift_rich_textTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
