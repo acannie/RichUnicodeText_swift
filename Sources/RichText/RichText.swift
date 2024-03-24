99b2f6a6f46ebe41cf2e8d80ec58755e5e0fb7f6
asasaoka <asasaoka@yahoo-corp.jp>
2024-03-24 16:03:11 +0900

    code: フォントの内容を小文字・大文字・数字・記号に分類してリファクタリング

--- Sources/RichText/RichText.swift
+++ Sources/RichText/RichText.swift
@@ -7,16 +7,46 @@
 
 public extension String {
     /// 英数字をリッチなテキストに変換
-    /// - Parameter font: 変換先のフォント
+    /// - Parameter targetFont: 変換先のフォント
     /// - Returns: リッチなテキスト
-    func richText(_ font: RichTextFontEnum) -> String {
+    func richText(_ targetFont: RichTextFontEnum) -> String {
         var richText = ""
         for c in self {
-            if let index = RichTextFontEnum.plain.rawValue.firstIndex(of: c) {
-                let indexInt = RichTextFontEnum.plain.rawValue.distance(from: RichTextFontEnum.plain.rawValue.startIndex, to: index)
-                let from = font.rawValue.index(font.rawValue.startIndex, offsetBy: indexInt)
-                richText += String(font.rawValue[from])
-            } else {
+            switch c {
+            case "a"..."z":
+                guard let plainLowerCases = RichTextFontEnum.plain.font.lowerCases,
+                      let targetFontLowerCases = targetFont.font.lowerCases else {
+                    richText += String(c)
+                    continue
+                }
+                if let index = plainLowerCases.firstIndex(of: c) {
+                    let indexInt = plainLowerCases.distance(from: plainLowerCases.startIndex, to: index)
+                    let from = targetFontLowerCases.index(targetFontLowerCases.startIndex, offsetBy: indexInt)
+                    richText += String(targetFontLowerCases[from])
+                }
+            case "A"..."Z":
+                guard let plainUpperCases = RichTextFontEnum.plain.font.upperCases,
+                      let targetFontUpperCases = targetFont.font.upperCases else {
+                    richText += String(c)
+                    continue
+                }
+                if let index = plainUpperCases.firstIndex(of: c) {
+                    let indexInt = plainUpperCases.distance(from: plainUpperCases.startIndex, to: index)
+                    let from = targetFontUpperCases.index(targetFontUpperCases.startIndex, offsetBy: indexInt)
+                    richText += String(targetFontUpperCases[from])
+                }
+            case "0"..."9":
+                guard let plainNumbers = RichTextFontEnum.plain.font.numbers,
+                      let targetFontNumbers = targetFont.font.numbers else {
+                    richText += String(c)
+                    continue
+                }
+                if let index = plainNumbers.firstIndex(of: c) {
+                    let indexInt = plainNumbers.distance(from: plainNumbers.startIndex, to: index)
+                    let from = targetFontNumbers.index(targetFontNumbers.startIndex, offsetBy: indexInt)
+                    richText += String(targetFontNumbers[from])
+                }
+            default:
                 richText += String(c)
             }
         }
