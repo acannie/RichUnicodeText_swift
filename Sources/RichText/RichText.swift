e4ec54f26343420edc7dd570348f6faf7157309b
asasaoka <asasaoka@yahoo-corp.jp>
2024-03-24 23:54:31 +0900

    code: コードを修正

--- Sources/RichText/RichText.swift
+++ Sources/RichText/RichText.swift
@@ -9,7 +9,7 @@ public extension String {
     /// 文字列をリッチなテキストに変換
     /// - Parameter targetFont: 変換先のフォント
     /// - Returns: リッチなテキスト
-    func richText(_ targetFont: RichTextFontEnum) -> String {
+    func richText(_ targetFont: RichUnicodeTextFontEnum) -> String {
         var richText = ""
         for c in self {
             richText += String(convertToRichCharacter(c, font: targetFont))
@@ -22,18 +22,18 @@ public extension String {
     ///  - c: 変換対象の文字
     ///  - targetFont: 変換先のフォント
     /// - Returns: リッチな文字
-    private func convertToRichCharacter(_ c: Character, font targetFont: RichTextFontEnum) -> Character {
+    private func convertToRichCharacter(_ c: Character, font targetFont: RichUnicodeTextFontEnum) -> Character {
         let plainFontString: String
         let targetTextString: String
         switch c {
         case "a"..."z":
-            plainFontString = RichTextFontEnum.plain.font.lowerCases
+            plainFontString = RichUnicodeTextFontEnum.plain.font.lowerCases
             targetTextString = targetFont.font.lowerCases
         case "A"..."Z":
-            plainFontString = RichTextFontEnum.plain.font.upperCases
+            plainFontString = RichUnicodeTextFontEnum.plain.font.upperCases
             targetTextString = targetFont.font.upperCases
         case "0"..."9":
-            plainFontString = RichTextFontEnum.plain.font.numbers
+            plainFontString = RichUnicodeTextFontEnum.plain.font.numbers
             targetTextString = targetFont.font.numbers
         default:
             return c
