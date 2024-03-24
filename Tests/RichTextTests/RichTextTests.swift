5d4cf55820e4219f12f0d8aa49ce2d78dff17101
asasaoka <asasaoka@yahoo-corp.jp>
2024-03-24 15:01:16 +0900

    code,test: 文字列をリッチテキストに変換する関数を実装

--- Tests/RichTextTests/RichTextTests.swift
+++ Tests/RichTextTests/RichTextTests.swift
@@ -2,11 +2,13 @@ import XCTest
 @testable import RichText
 
 final class richTextTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    func test_convertToMathematicalBold() {
+        // 準備
+        let originalString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+        let expectedString = "𝐚𝐛𝐜𝐝𝐞𝐟𝐠𝐡𝐢𝐣𝐤𝐥𝐦𝐧𝐨𝐩𝐪𝐫𝐬𝐭𝐮𝐯𝐰𝐱𝐲𝐳𝐀𝐁𝐂𝐃𝐄𝐅𝐆𝐇𝐈𝐉𝐊𝐋𝐌𝐍𝐎𝐏𝐐𝐑𝐒𝐓𝐔𝐕𝐖𝐗𝐘𝐙𝟏𝟐𝟑𝟒𝟓𝟔𝟕𝟖𝟗𝟎"
+        // 実行
+        let result = originalString.richText(.mathematicalBold)
+        // 検証
+        XCTAssertEqual(expectedString, result)
     }
 }
